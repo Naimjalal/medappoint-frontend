47092fc476c9a666ba8cfdcd1490de736a477b3d
naimjalal <naimjalal@gmail.com>
2025-06-04 15:50:26 +0300

    created routes pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import Nav from './components/Nav'
 import { Route, Routes } from 'react-router'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
 
 const App = () => {
   return (
@@ -7,9 +10,9 @@ const App = () => {
       <Nav />
       <main>
         <Routes>
-          <Route path="/" />
-          <Route path="/signin" />
-          <Route path="/register" />
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </main>
     </div>
