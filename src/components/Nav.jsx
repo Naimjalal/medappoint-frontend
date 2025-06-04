173e2a3464289f18ba8e84c938997f84921beaf6
naimjalal <naimjalal@gmail.com>
2025-06-04 14:23:47 +0300

    created nav bar links

---
+++ src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+import "./Nav.css"
+const Nav = ({ user, handleLogOut }) => {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      {user ? (
+        <>
+          <span>Welcome, {user.email}</span>
+          <button onClick={handleLogOut}>Log Out</button>
+        </>
+      ) : (
+        <>
+          <Link to="/register">Register</Link>
+          <Link to="/signin">Sign In</Link>
+        </>
+      )}
+    </nav>
+  )
+}
+
+export default Nav
