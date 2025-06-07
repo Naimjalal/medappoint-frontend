efe8f009f603ddca4b4870fd94043839a4160015
naimjalal <naimjalal@gmail.com>
2025-06-07 15:22:28 +0300

    implemented frontend sign in user

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Nav from './components/Nav'
 import { Route, Routes } from 'react-router'
 import Register from './pages/Register'
@@ -5,13 +6,14 @@ import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import Hospital from './pages/Hospital'
 const App = () => {
+  const [user,setUser] = useState(null)
   return (
     <div>
-      <Nav />
+      <Nav  user={user} handleLogOut={()=> setUser(null)}/>
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signin" element={<SignIn setUser={setUser}/>} />
           <Route path="/register" element={<Register />} />
           <Route path="/hospitals/:hospitalId" element={<Hospital />} />
         </Routes>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,46 @@
-const SignIn = () => {
+import { useState } from 'react'
+import axios from 'axios'
+const SignIn = ({setUser}) => {
+  const [formState, setFormState] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (e) => {
+    setFormState({...formState,[e.target.name]: e.target.value})
+  }
+  const handleSubmit = async(e) =>{
+    e.preventDefault()
+    try {
+      const response = await axios.post("http://localhost:3001/auth/login",formState)
+      localStorage.setItem("token", response.data.token)
+      setUser(response.data.user)
+      setFormState({email: "", password:""})
+      console.log("Login Successful")
+    } catch (error) {
+      console.error("Login failed",error.response?.data || error.message)
+    }
+  }
+
   return (
-    <div>
-      <h2>Sign In Page</h2>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h2>Sign In </h2>
+      <input
+      type = "email"
+      name='email'
+      placeholder='Email'
+      value={formState.email}
+      onChange={handleChange}
+      />
+      <input
+      type='password'
+      name="password"
+      placeholder='Password'
+      value={formState.password}
+      onChange={handleChange}
+      />
+      <button type='submit'>Login</button>
+    </form>
   )
 }
 
